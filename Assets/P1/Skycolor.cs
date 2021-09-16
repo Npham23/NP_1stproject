f3543df3c109bad568abb8e2b1e692427f1bc224
Npham23 <82294334+Npham23@users.noreply.github.com>
2021-09-16 16:31:06 -0400

    Initial commit

---
+++ Assets/P1/Skycolor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skycolor : MonoBehaviour
+{
+    public Gradient cycleColors;
+
+    public Camera mainCamera;
+    public Color startColor;
+    public Color midColor;
+    public Color endColor;
+    public float fullyCycleTime = 18;
+    float currentLerping = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.backgroundColor = startColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Method A
+        //Manually Inverse the lerp the value
+        //currentLerping = (1 / (fullyCycleTime / 3)) * Time.deltaTime;
+
+        //Method B
+        // Use Inverse lerp and calculate how much time has passed sice the start of the cycle
+        // by multiplying it for how long a section of said cycle is
+        currentLerping = Mathf.InverseLerp(0, fullyCycleTime / 3, currentLerping * Time.deltaTime);
+        //Method C
+        // keep track of the time and simply inverse lerp it
+        
+
+        if (currentLerping >= 0 && currentLerping < (1f/3))
+        {
+            Camera.main.backgroundColor = Color.Lerp(startColor, midColor, currentLerping * 3);
+
+        }
+        else if(currentLerping >= 1 && currentLerping < (2f/3))
+        {
+            Camera.main.backgroundColor = Color.Lerp(midColor, endColor, currentLerping - ((1f/3) * 3));
+        }
+        else if (currentLerping >= 2 && currentLerping < (3f/3))
+        {
+            Camera.main.backgroundColor = Color.Lerp(endColor, startColor, currentLerping - ((2f / 3) * 3));
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/P1/e.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class e : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+  //  public float rotationLength = 10;
+  //  public Vector3 startAngle;
+  //  public Vector3 endAngle;
+    public float eAngle;
+    public float sAngle;
+   // Vector3 curve1 = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 angle = this.transform.eulerAngles;
+        angle.z = sAngle;
+        this.transform.eulerAngles = angle;
+        //this.transform.eulerAngles = startAngle;
+    }
+
+    // Update is called once per frame      
+    void Update()
+    {
+        Vector3 tempAngle = this.transform.eulerAngles;
+        //  teamAngle.z = tempAngle.z + rotationSpeed;
+        tempAngle.z = tempAngle.z + rotationSpeed + Time.deltaTime;
+
+        this.transform.eulerAngles = tempAngle;
+      //  this.transform.eulerAngles = Vector3.SmoothDamp(this.transform.eulerAngles, endAngle, ref curve1, rotationSpeed);
+    }
+}
